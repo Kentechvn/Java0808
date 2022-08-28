a832fa513990f3300e440bfe4a499cea5b894461
kentechvn <kenny012487@gmail.com>
2022-08-28 13:18:51 +0700

    Scanner

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -1,19 +1,43 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Buoi1 {
     public static void main(String[] args) {
-  //      int c = -5;
-  //      int d = 5;
-   //     int result = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
-  //      System.out.println("Gia tri cua ket qua: " + result);
+        //      int c = -5;
+        //      int d = 5;
+        //     int result = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
+        //      System.out.println("Gia tri cua ket qua: " + result);
 
-     //   int a = 5;
-    //   int b = 3;
-     //  int result = a++ - --b + --a + a-- + b++ - b-- + a++ + --a;
-      // System.out.println("Gia Tri Cua Bieu Thuc Bang " +result);
-      //  int c = -5;
-      //  int d = 5;
-     //   int result2 = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
-     //   System.out.println("Gia Tri Cua result la " +result2);
+        //   int a = 5;
+        //   int b = 3;
+        //  int result = a++ - --b + --a + a-- + b++ - b-- + a++ + --a;
+        // System.out.println("Gia Tri Cua Bieu Thuc Bang " +result);
+        //  int c = -5;
+        //  int d = 5;
+        //   int result2 = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
+        //   System.out.println("Gia Tri Cua result la " +result2);
+// int a = 5;
+// int b = a % 3;
+
+// System.out.println("Gia Tri Cua b là" +b);
+       // int a = 3;
+       // int b = a - 2 + 2 -1;
+      //  if (a < b) {
+      //      System.out.println("a nho hon b");
+     //   } else if (a > b) {
+     //       System.out.println("a lon hon b");
+     //   } else if (a == b) {
+     //       System.out.println("a bang b");
+      //  }
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Moi Ban Nhap Ten: ");
+            String name = scanner.nextLine();
+                    System.out.print("Ten Ban La " + name);
+            System.out.print("Moi Ban Nhap Tuoi: ");
+            String age = scanner.nextLine();
+            System.out.print("Tuoi Cua Ban La " + age);
+        }
+        }
 
     }
-}
