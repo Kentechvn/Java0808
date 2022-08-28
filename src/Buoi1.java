067adcee329e6be70125a4c0404d970de238a902
kentechvn <kenny012487@gmail.com>
2022-08-28 11:04:32 +0700

    Toan Tu

---
+++ src/Buoi1.java
@@ -0,0 +1,19 @@
+
+public class Buoi1 {
+    public static void main(String[] args) {
+  //      int c = -5;
+  //      int d = 5;
+   //     int result = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
+  //      System.out.println("Gia tri cua ket qua: " + result);
+
+     //   int a = 5;
+    //   int b = 3;
+     //  int result = a++ - --b + --a + a-- + b++ - b-- + a++ + --a;
+      // System.out.println("Gia Tri Cua Bieu Thuc Bang " +result);
+      //  int c = -5;
+      //  int d = 5;
+     //   int result2 = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
+     //   System.out.println("Gia Tri Cua result la " +result2);
+
+    }
+}
