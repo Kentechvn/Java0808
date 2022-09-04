18d9e4156f0bc9be73964d71228d63d9ad3bfa0f
kentechvn <kenny012487@gmail.com>
2022-09-04 15:42:17 +0700

    array

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -1,48 +1,83 @@
-import java.text.DecimalFormat;
-import java.util.Scanner;
+
 
 public class Buoi1 {
-<<<<<<< HEAD
-    public static void main(String[] args)
-    //thay doi them
-    //hello
-  //      int c = -5;
-  //      int d = 5;
-   //     int result = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
-  //      System.out.println("Gia tri cua ket qua: " + result);
-=======
->>>>>>> 39394c786189eb0770bc131379ff24ce2f791a7d
+    public static void main(String[] args) {
 
-        //   int a = 5;
-        //   int b = 3;
-        //  int result = a++ - --b + --a + a-- + b++ - b-- + a++ + --a;
-        // System.out.println("Gia Tri Cua Bieu Thuc Bang " +result);
-        //  int c = -5;
-        //  int d = 5;
-        //   int result2 = --c + c++ - d-- + d-- + ++c + c-- - d++ + c++ - d--;
-        //   System.out.println("Gia Tri Cua result la " +result2);
-// int a = 5;
-// int b = a % 3;
+        // System.out.println("line1\nline2\nline3"); dùng\n để in liên tục
+/*
+        System.out.println(313 + 122);
+        Có thể thực hiện phép tính trong lệnh print.
+        System.out.println("313 - 212 = " + (313 - 212));
 
-// System.out.println("Gia Tri Cua b là" +b);
-       // int a = 3;
-       // int b = a - 2 + 2 -1;
-      //  if (a < b) {
-      //      System.out.println("a nho hon b");
-     //   } else if (a > b) {
-     //       System.out.println("a lon hon b");
-     //   } else if (a == b) {
-     //       System.out.println("a bang b");
-      //  }
-        {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Moi Ban Nhap Ten: ");
-            String name = scanner.nextLine();
-                    System.out.print("Ten Ban La " + name);
-            System.out.print("Moi Ban Nhap Tuoi: ");
-            String age = scanner.nextLine();
-            System.out.print("Tuoi Cua Ban La " + age);
-        }
-        }
+        // Khai báo biến a kiểu số nguyên
+		int a;
+		// Gán giá trị cho a = 5
+		a = 5;
+		// Hiển thị ra màn hình giá trị của biến a
+		System.out.println("a = " + a);
+		int a = 234, b = 432;  //bạn cũng có thể khai báo và gán giá trị cho nhiều biến trong 1 câu lệnh
+        System.out.println("a + b = " + (a + b));
+        String name = "Codelearn";
+        int dob = 2019;
+        String line1 = "name: " +name;
+        String line2 = "Date of Birth: " +dob;
+        System.out.println("name: " +name);
+        System.out.println("Date of Birth: " +dob);
+*/
+       /* double a = 3;
+        double b = 4;
+        double c = b / a; //Kết quả sẽ là 1 mà không phải 1.(3) do cả a và b đều là biến kiểu số nguyên nên kết quả sẽ là một số nguyên. Do đó khi thực hiện các phép toán trong Java bạn cần lưu ý tới kiêu dữ liệu.
+        System.out.println(c);
+        */
 
+       /* double a = 1.5;
+        int b = 7;
+        System.out.println(a / b);
+
+        Đoạn code mẫu chưa hiểu:
+        #include<iostream>
+
+using namespace std;
+
+int main() {
+	double a = 10.5;
+	double b = 7;
+	cout << "a / b = " << a / b;
+	return 0;
+}
+        */
+   // Double a = 7.5;
+    //Double b = 3.8;
+    //System.out.println("Area = " + (a * b));
+      //  char c = 'x';
+    //    System.out.println(c);
+       // char d = 'x';
+       // System.out.println(d);
+/*
+        long a = 384847522; //long là kểu dài hơn int
+        long b = 988347273;
+        System.out.println(" a x b = " + (a * b));
+
+Hoặc    int a = 384847522;
+        int b = 988347273;
+        System.out.println((long) a * b);
+
+ */
+
+   // boolean b = true;
+    // System.out.println(b);
+/* Cách 1: tạo ra và truyền phần tử cho mảng
+    String[]arrNames= {"Teo","Ti","Tun","Lan","Mai"};
+    // System.out.println("Kich thuoc cua mang la " + arrNames.length);
+    // Lây giá trị phần tử theo index
+        // Vị trí đầu tiên sẽ có index = 0
+        // Vị trí cuối: arrNames.length - 1
+       // System.out.println(arrNames[1]);
+        arrNames[1] = "Ti Updated"; // "Gán lại giá trị của index 1 khác"
+        System.out.println(arrNames[1]);
+
+ */
+        // Cách 2: Tạo ra mảng nhưng chỉ biết số lượng
+        // String[] arrnames = new String[10]
     }
+}
